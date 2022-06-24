66949f2de5edd56837f7642416cbe7913ea29db1
Hao Chen <61271027+bilibalaboom413@users.noreply.github.com>
2022-06-24 11:22:40 +0800

    Finish User

--- src/features/posts/PostList.js
+++ src/features/posts/PostList.js
@@ -1,14 +1,17 @@
 import { useSelector } from "react-redux";
 // import { article } from "txtgen/types";
 import { Link } from "react-router-dom";
+import { PostAuthor } from "./PostAuthor";
 
 export const PostList = () => {
   const posts = useSelector((state) => state.posts);
+  console.log(posts);
 
   const renderedPosts = posts.map((post) => (
     <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
       <p className="post-content">{post.content.substring(0, 100)}</p>
+      <PostAuthor userId={post.user} /> <br />
       <Link to={`/posts/${post.id}`} className="button muted-button">
         View Post
       </Link>
